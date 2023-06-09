c676b891403cda7c646c5a34cce3f06f7b4287b9
Konrad Thiemann <konrad.gruss@t-online.de>
2023-06-09 15:01:58 +0200

    first commit/header

---
+++ portfolio/src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  selectLink(num: number){
+    let links = document.getElementsByClassName('link-underline');
+    for(let i = 0; i < links.length; i++){
+      document.getElementById('link' + i)!.style.color = 'white';
+      links[i].classList.remove('active');
+    }
+    links[num].classList.add('active');
+    document.getElementById('link' + num)!.style.color = '#70E61C';
+  }
+}
